05f6ecaf3c31309dfec63513bfbb6704fbb110ed
siuwafong <wilsonfong82@gmail.com>
2021-07-20 00:20:17 -0400

    change url in socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,12 +1,12 @@
-const jwt = require("jsonwebtoken");
-const socket = require("socket.io");
-const onlineUsers = require("../onlineUsers/onlineUsers");
-const socketCookieParser = require("socket.io-cookie-parser");
+const jwt = require('jsonwebtoken');
+const socket = require('socket.io');
+const onlineUsers = require('../onlineUsers/onlineUsers');
+const socketCookieParser = require('socket.io-cookie-parser');
 
 exports.appSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: 'https://happy-heyrovsky-5a1d90.netlify.app',
       credentials: true,
     },
   });
@@ -15,7 +15,7 @@ exports.appSocket = (server) => {
 
   // jwt authentication
   io.use((socket, next) => {
-    const requestedToken = socket.request.cookies["token"];
+    const requestedToken = socket.request.cookies['token'];
     if (!requestedToken) {
       return next(new Error(`There is no token Found!`));
     } else {
@@ -28,27 +28,27 @@ exports.appSocket = (server) => {
     }
   });
 
-  io.on("connection", (socket) => {
-    console.log("User", socket.id, "Connected");
+  io.on('connection', (socket) => {
+    console.log('User', socket.id, 'Connected');
 
-    socket.on("notification", ({ type, sender, recipient }) => {
-      socket.broadcast.emit("notification", {
+    socket.on('notification', ({ type, sender, recipient }) => {
+      socket.broadcast.emit('notification', {
         type,
         from: sender,
         to: recipient,
       });
     });
 
-    socket.on("clearNotification", ({ type, sender, recipient }) => {
-      socket.emit("notification", {
+    socket.on('clearNotification', ({ type, sender, recipient }) => {
+      socket.emit('notification', {
         type,
         from: sender,
         to: recipient,
       });
     });
 
-    socket.on("message", ({ sender, recipient }) => {
-      socket.broadcast.emit("message", {
+    socket.on('message', ({ sender, recipient }) => {
+      socket.broadcast.emit('message', {
         from: sender,
         to: recipient,
       });
