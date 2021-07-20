ea1d46f6a6e7e53da768da1abe82f78fa96c55a3
Wilson Fong <wilsonfong82@gmail.com>
2021-07-20 13:56:13 -0400

    remove production if statement

--- app.js
+++ app.js
@@ -150,17 +150,17 @@ app.use('/message', messageRouter);
 app.use('/payment', paymentRouter);
 app.use('/review', reviewRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/client/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html')
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running');
-  });
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html')
+//   );
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running');
+//   });
+// }
 
 app.use(notFound);
 app.use(errorHandler);
